724bc69fc7c752c148b2150856acc3c984e0f73b
Deon Levon Dmello <80735760+lekenzi@users.noreply.github.com>
2024-04-22 22:30:50 +0530

    add images

--- image_prcessor/image_preprocessor.py
+++ image_prcessor/image_preprocessor.py
@@ -11,24 +11,29 @@ def load_images_from_dir(directory):
     for image_path in image_paths:
         image = tf.io.read_file(image_path)
         image = tf.image.decode_image(image, channels=3)
-        image = tf.image.resize(image, [224, 224])  # Adjust the size as needed
-        image = image / 255.0  # Normalize the pixel values
+        image = tf.image.resize(image, [128, 128])  # Adjust the size to 128x128
         images.append(image)
     
     return images
 
 
-def preprocess_images(images, batch_size):
-    # Convert images to black and white
-    bw_images = []
-    for image in images:
-        bw_image = tf.image.rgb_to_grayscale(image)
-        bw_images.append(bw_image)
+def preprocess_images(x_images, y_images, batch_size):
+    # Combine x_images and y_images into a single dataset
+    dataset = tf.data.Dataset.from_tensor_slices((x_images, y_images))
     
-    # Create a dataset from the black and white images
-    dataset = tf.data.Dataset.from_tensor_slices(bw_images)
+    # Preprocess each image in the dataset
+    def preprocess_image(x_image, y_image):
+        x_image = tf.io.read_file(x_image)
+        x_image = tf.image.decode_image(x_image, channels=3)
+        x_image = tf.image.resize(x_image, [128, 128])
+        
+        y_image = tf.io.read_file(y_image)
+        y_image = tf.image.decode_image(y_image, channels=3)
+        y_image = tf.image.resize(y_image, [128, 128])
+        
+        return x_image, y_image
     
-    # Prefetch the dataset
+    dataset = dataset.map(preprocess_image)
     dataset = dataset.batch(batch_size).prefetch(tf.data.experimental.AUTOTUNE)
     
     return dataset
--- train_model/train.py
+++ train_model/train.py
@@ -1,8 +1,15 @@
 from bw_to_color_model.colorizer_model import ECCVGenerator
 
 
-def train_model(X_train, y_train, epochs=10, batch_size=32):
+def train_model(dataset, epochs=10, batch_size=32):
     model = ECCVGenerator()
     model.compile(optimizer='adam', loss='mse')
     model.summary()
-    model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size)
+    history = model.fit(dataset, epochs=epochs, batch_size=batch_size)
+    
+    # Save the model
+    model.save('/path/to/save/model.h5')
+    
+    # Save the history object
+    with open('/history/history.obj', 'wb') as file:
+        pickle.dump(history.history, file)
